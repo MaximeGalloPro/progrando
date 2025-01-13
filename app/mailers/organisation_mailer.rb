c28662b2aa7391ddc765589fd794241e59175675
Maxime Gallo <pro.maxgallo@gmail.com>
2025-01-13 18:45:33 +0100

    mailer working

--- app/mailers/organisation_mailer.rb
+++ app/mailers/organisation_mailer.rb
@@ -3,9 +3,12 @@ class OrganisationMailer < ApplicationMailer
         @access_request = access_request
         @user = access_request.user
         @organisation = access_request.organisation
-
+        profile_right = ProfileRight.where(resource: 'ProfileRight', action: 'toggle_authorization', authorized: true)
+        profile_ids = profile_right.pluck(:profile_id)
+        user_ids = UserOrganisation.where(organisation_id: @organisation.id, profile_id: profile_ids).pluck(:user_id)
+        users = User.where(id: user_ids)
         mail(
-            to: @organisation.admins.pluck(:email),
+            to: users.pluck(:email),
             subject: "Nouvelle demande d'accès à #{@organisation.name}"
         )
     end
